c2658f9a8c862e7ae563ddff9adb914c1210e55b
Rojin Bijukchhe <ro_jin25@icloud.com>
2024-02-26 11:43:45 -0600

    Handling Async eroor.

--- app.ts
+++ app.ts
@@ -10,13 +10,37 @@ function B() {
   /** not find error */
   // console.log(x);
 }
+function foo() {
+  return Promise.reject(new Error("ohh"));
+}
 
-try {
-  "hello".normalize("theo");
-  //   throw new Error("Eorrorrrrr");
-  //   throw "Somethind went wrong";
-} catch (e) {
-  console.log("/.|");
-  //   console.log("Something went worng.");
-  console.log(e);
+class TheoError extends Error {
+  constructor(message: string, public msg: string) {
+    super(msg);
+  }
+}
+
+async function foo2() {
+  try {
+    await foo();
+    throw new TheoError("hello", "good boy");
+    //   "hello".normalize("theo");
+    //   throw new Error("Eorrorrrrr");
+    //   throw "Somethind went wrong";
+  } catch (e: unknown) {
+    // console.log("When used any we can use:", e.potato);
+    //   console.log("/.|");
+    //   console.log("Something went worng.");
+    if (e instanceof TheoError) {
+      // use "if" to control unknown.
+      console.log(e.msg);
+    }
+    if (e instanceof Error) {
+      console.log(e.message);
+    } else if (typeof e === "string") {
+      console.log("something went wrong with string ");
+    } else {
+      console.log("something went wrong");
+    }
+  }
 }
