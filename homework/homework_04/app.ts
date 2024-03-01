9910e0eb0d706ef201ea74029ca46a07fa5bf8e2
Rojin Bijukchhe <ro_jin25@icloud.com>
2024-02-29 21:52:59 -0600

    reading files Input and output sucessfully.

--- homework/homework_04/app.ts
+++ homework/homework_04/app.ts
@@ -1,1 +1,18 @@
+import { readdirSync } from "node:fs";
+import { join } from "node:path";
+
+const pathToIn = "./input";
+const pathToOut = "./output";
+
+// readdirSync(pathToFolder);
+const inInputDir = readdirSync(pathToIn).map((fileName) => {
+  return join(pathToIn, fileName);
+});
+const inOutputDir = readdirSync(pathToOut).map((fileName) => {
+  return join(pathToOut, fileName);
+});
+
+console.log(inInputDir);
+console.log(inOutputDir);
+
 console.log("app connected");
