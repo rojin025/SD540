466a4ed061002862b3780b2167d594f9b8892a3c
Rojin Bijukchhe <ro_jin25@icloud.com>
2024-02-29 22:52:30 -0600

    Completed.

--- homework/homework_04/app.ts
+++ homework/homework_04/app.ts
@@ -1,18 +1,43 @@
 import { readdirSync } from "node:fs";
 import { join } from "node:path";
+import sharp from "sharp";
 
 const pathToIn = "./input";
 const pathToOut = "./output";
 
 // readdirSync(pathToFolder);
 const inInputDir = readdirSync(pathToIn).map((fileName) => {
-  return join(pathToIn, fileName);
+  return join(fileName);
 });
 const inOutputDir = readdirSync(pathToOut).map((fileName) => {
-  return join(pathToOut, fileName);
+  return join(fileName);
 });
 
-console.log(inInputDir);
-console.log(inOutputDir);
+const notInOutput = inInputDir.filter((img) => !inOutputDir.includes(img));
+
+console.log("Files in input directory:", inInputDir);
+console.log("Files in output directory:", inOutputDir);
+console.log("Files not in output directory:", notInOutput);
 
 console.log("app connected");
+
+function convertToThum(notInOut: string[]) {
+  notInOut.forEach((img) => {
+    const inputFilePath = join(pathToIn, img); // Full path to input file
+    const outputFileName = join(pathToOut, img.split("/").pop() || ""); // Full path to output file
+    console.log("Input file path:", inputFilePath);
+    console.log("Output file path:", outputFileName);
+
+    sharp(inputFilePath)
+      .resize({ width: 200 })
+      .toFile(outputFileName, (err, info) => {
+        if (err) {
+          console.error(`Error resizing ${img}:`, err);
+        } else {
+          console.log(`Resized ${img} to ${info.width}x${info.height}`);
+        }
+      });
+  });
+}
+
+convertToThum(notInOutput);
