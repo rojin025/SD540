49bd29fa9ede360d4d4c2804b6c1f450bbc6fbc4
Rojin Bijukchhe <ro_jin25@icloud.com>
2024-03-02 13:59:02 -0600

    Exam prep 1.0.1

--- exam.ts
+++ exam.ts
@@ -1,1 +1,12 @@
 console.log("Exam Running");
+
+(() => new Promise((resolve) => resolve("promise")))()
+  .then((p) => console.log(p))
+  .then(() => {
+    setTimeout(() => console.log("timeout"), 0);
+    setImmediate(() => console.log("immediate"));
+    queueMicrotask(() => console.log("microtask"));
+    process.nextTick(() => console.log("nexttick"));
+  });
+
+// ex2
