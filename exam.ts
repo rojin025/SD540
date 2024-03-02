24dec61124f51581f60ccdf002e936d98949368d
Rojin Bijukchhe <ro_jin25@icloud.com>
2024-03-02 14:01:33 -0600

    Exam prep 1.0.2

--- exam.ts
+++ exam.ts
@@ -1,12 +1,20 @@
 console.log("Exam Running");
 
-(() => new Promise((resolve) => resolve("promise")))()
-  .then((p) => console.log(p))
-  .then(() => {
-    setTimeout(() => console.log("timeout"), 0);
-    setImmediate(() => console.log("immediate"));
-    queueMicrotask(() => console.log("microtask"));
-    process.nextTick(() => console.log("nexttick"));
-  });
+// //ex1
+// (() => new Promise((resolve) => resolve("promise")))()
+//   .then((p) => console.log(p))
+//   .then(() => {
+//     setTimeout(() => console.log("timeout"), 0);
+//     setImmediate(() => console.log("immediate"));
+//     queueMicrotask(() => console.log("microtask"));
+//     process.nextTick(() => console.log("nexttick"));
+//   });
 
 // ex2
+(() => new Promise((resolve) => resolve("promise")))().then((p) =>
+  console.log(p)
+);
+setTimeout(() => console.log("timeout"), 0);
+setImmediate(() => console.log("immediate"));
+queueMicrotask(() => console.log("microtask"));
+process.nextTick(() => console.log("nexttick"));
