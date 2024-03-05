9ef63f11846445b8ae2ed3d1c84b02af68412054
Rojin Bijukchhe <ro_jin25@icloud.com>
2024-03-05 11:56:52 -0600

    Model operations - 1.0.2.

--- app_week2.ts
+++ app_week2.ts
@@ -1,28 +1,28 @@
 import mongoose, { Schema, model, InferSchemaType } from "mongoose";
 
-/**
- * localhost === 127.0.0.1
- */
-mongoose
-  .connect("mongodb://127.0.0.1:27017/demo")
-  .then((_) => console.log("Connceted to DB"))
-  .catch(console.log);
+// /**
+//  * localhost === 127.0.0.1
+//  */
+// mongoose
+//   .connect("mongodb://127.0.0.1:27017/demo")
+//   .then((_) => console.log("Connceted to DB"))
+//   .catch(console.log);
 
-/**
- * 1 create schemma
- *
- * infer a type from a schemma
- */
+// /**
+//  * 1 create schemma
+//  *
+//  * infer a type from a schemma
+//  */
 
-const userSchema = new Schema({});
+// const userSchema = new Schema({});
 
-type User = InferSchemaType<typeof userSchema>;
-const userModel = model<User>("user", userSchema);
+// type User = InferSchemaType<typeof userSchema>;
+// const userModel = model<User>("user", userSchema);
 
-/**
- * To disconnect
- * mongoose.disconnect()
- */
+// /**
+//  * To disconnect
+//  * mongoose.disconnect()
+//  */
 
 // create a schema
 const schema = new Schema(
@@ -37,7 +37,52 @@ const schema = new Schema(
   },
   { timestamps: true }
 );
-// create an inferred type from your schema
+
+/**
+ * create an inferred type from your schema (Structure).
+ */
 export type User2 = InferSchemaType<typeof schema>;
-// create a model object using schema
+
+/**
+ * create a model object using schema
+ * Collections -> Users
+ */
 const userModel2 = model<User2>("user", schema);
+
+/**
+ * we work with model for CRUD operation.
+ * methods used in model return promises
+ * model: collection
+ * doc: instance of model is doc
+ */
+
+async function run() {
+  //   const user: User2 = await userModel2.create({
+  //     userName: "mike",
+  //     password: "123",
+  //   });
+  //   console.log(user);
+
+  /**
+   * first parm is query
+   * sec param is projection
+   * $in means or
+   * , -> && // after $in
+   * $in in same field
+   * $or in different field [
+        { weight: { $lt: 20 } },
+        { height: 10 }]
+   */
+  const user: User2 | null = await userModel2.findOne(
+    {
+      weight: { $lte: 100 },
+    },
+    {
+      password: 1,
+      //   _id = 0,
+    }
+  );
+  console.log(user);
+}
+
+run();
